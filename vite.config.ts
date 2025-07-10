1141c6c1e1d6ae600663254ae2f41123e711fb27
amanharma468 <132251957+amanharma468@users.noreply.github.com>
2025-07-10 13:10:53 +0530

    chunk size updated fro 500 to 1000

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // Suppress warning for chunks up to 1MB
+  },
 }));
